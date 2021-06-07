80e765a9004d8e7cbbbdcf1cd35a86f2b4b61894
[Thinh-Ngo] <[thinhnt@mobile-id.vn]>
2021-06-07 10:59:19 +0700

    optimize template

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from .views import index
 
 urlpatterns = [
+    path('', index, name='index'),
     path('accounts/', include('allauth.urls')),
     path('admin/', admin.site.urls),
 ]
@@ -11,5 +13,5 @@ urlpatterns = [
 # if settings.DEBUG:
 #     urlpatterns += static(settings.STATIC_URL,
 #                           document_root=settings.STATIC_ROOT)
-#     urlpatterns += static(settings.MEDIA_URL,
-#                           document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL,
+#                       document_root=settings.MEDIA_ROOT)
