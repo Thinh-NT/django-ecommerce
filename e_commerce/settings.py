52e5ec48f3056d3e29527ff0c3eb7b3900a5c1aa
[Thinh-Ngo] <[thinhnt@mobile-id.vn]>
2021-06-18 10:22:43 +0700

    update views

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -111,12 +111,22 @@ WSGI_APPLICATION = 'e_commerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'store',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': '110996',
+    }}
 
 
 # Password validation
